7d9d1b048dfe5c897f2ba60c6f696cd53fe4c739
Tonni1973 <tonniramdani@gmail.com>
2025-12-08 08:27:04 +0700

    day 7 java basic

---
+++ Language/Java/Basic/JavaLanguage/src/BreakDanContinue.java
@@ -0,0 +1,23 @@
+public class BreakDanContinue {
+
+    // break digunakan untuk menyelesaikan perulangan ketika ada kondisi tertentu
+   // continue digunakan untuk melewati perulangan ketika ada kondisi tertentu
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            if (i == 5){
+                System.out.println(i);
+                break;
+            }
+            System.out.println("Hello");
+        }
+
+        for (int i = 0; i < 10; i++) {
+            // print hanya bilangan genap
+            if (i % 2 != 0){
+                continue;
+            }
+            System.out.println(i);
+
+        }
+    }
+}
