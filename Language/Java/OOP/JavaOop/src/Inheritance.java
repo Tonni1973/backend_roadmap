c7cc0ed994b958fb8564d042358fdd4646fe3923
Tonni1973 <tonniramdani@gmail.com>
2025-12-14 14:30:58 +0700

    day 13 java oop

--- Language/Java/OOP/JavaOop/src/Inheritance.java
+++ Language/Java/OOP/JavaOop/src/Inheritance.java
@@ -2,10 +2,12 @@ class Manager{
     String name;
 
     void sayHello(String name){
-        System.out.println("Hello " + name + ", My Name is " + this.name);
+        System.out.println("Hello " + name + ", My Name is Manager" + this.name);
     }
 }
 
 class VicePresident extends Manager {
-
+    void sayHello(String name){
+        System.out.println("Hello " + name + ", My Name is VicePresident "+ this.name);
+    }
 }
