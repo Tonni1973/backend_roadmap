46e9338fe3b0925c2bce39de9563880592a70867
Tonni1973 <tonniramdani@gmail.com>
2025-12-18 12:24:00 +0700

    day 16 java oop

--- Language/Java/OOP/JavaOop/src/PolymorphismApp.java
+++ Language/Java/OOP/JavaOop/src/PolymorphismApp.java
@@ -1,6 +1,7 @@
 public class PolymorphismApp {
     public static void main(String[] args) {
         Employee employee= new Employee("Tonni");
+        // method polymorph
         employee.sayHello(new Manager("eko"));
 
         // polymorph
