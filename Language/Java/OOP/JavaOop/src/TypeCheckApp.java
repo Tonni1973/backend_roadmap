59e028ec3a6423e45c44999ccf2e9436088d5914
Tonni1973 <tonniramdani@gmail.com>
2025-12-20 04:53:14 +0700

    day 17 java oop

---
+++ Language/Java/OOP/JavaOop/src/TypeCheckApp.java
@@ -0,0 +1,17 @@
+public class TypeCheckApp {
+    public static void main(String[] args) {
+        sayHello(new VicePresident("Davin"));
+    }
+
+    static void sayHello(Employee employee){
+        if(employee instanceof VicePresident) {
+            VicePresident vicePresident = (VicePresident) employee;
+            System.out.println("Hello VP "+ vicePresident.name);
+        } else if (employee instanceof  Manager) {
+            Manager manager = (Manager) employee;
+            System.out.println("Hello Manager " + manager.name);
+        } else {
+            System.out.println("Hello VP "+ employee.name);
+        }
+    }
+}
