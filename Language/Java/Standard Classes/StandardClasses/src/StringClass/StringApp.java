36284a86fc035e7fcb6f40c1c5679965617b6b9d
Tonni1973 <tonniramdani@gmail.com>
2026-01-03 05:24:28 +0700

    day 31 java standard classes

---
+++ Language/Java/Standard Classes/StandardClasses/src/StringClass/StringApp.java
@@ -0,0 +1,9 @@
+package StringClass;
+
+public class StringApp {
+    public static void main(String[] args) {
+        String name = "Toni";
+
+        System.out.println(name.toLowerCase());
+    }
+}
