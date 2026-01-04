3edfd7dd1536be493ae9dbdc0957af7e0307375c
Tonni1973 <tonniramdani@gmail.com>
2026-01-04 09:12:03 +0700

    day 32 standard classes

--- Language/Java/Standard Classes/StandardClasses/src/StringClass/StringApp.java
+++ Language/Java/Standard Classes/StandardClasses/src/StringClass/StringApp.java
@@ -2,8 +2,14 @@ package StringClass;
 
 public class StringApp {
     public static void main(String[] args) {
-        String name = "Toni";
+        String name = "Nuna";
 
         System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.length());
+        System.out.println(name.startsWith("Nu"));
+
+        String[] split = name.split("N");
+        System.out.println(split[1]);
     }
 }
