62d63545bbc9a722eaf547f2cbc6cf877ca987b6
Tonni1973 <tonniramdani@gmail.com>
2026-01-09 08:48:15 +0700

    day 36 java standard classes

---
+++ Language/Java/Standard Classes/StandardClasses/src/BigNumber/BigNumberApp.java
@@ -0,0 +1,15 @@
+package BigNumber;
+
+import java.math.BigInteger;
+
+public class BigNumberApp {
+    public static void main(String[] args) {
+        BigInteger a =  new BigInteger("10000000000000000");
+        BigInteger b =  new BigInteger("10000000000000000");
+
+        BigInteger jumlah = a.add(b);
+
+        System.out.println(jumlah);
+
+    }
+}
---
+++ Language/Java/Standard Classes/StandardClasses/src/MathClass/MathClassApp.java
@@ -0,0 +1,12 @@
+package MathClass;
+
+public class MathClassApp {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.min(a, b));
+
+    }
+}
