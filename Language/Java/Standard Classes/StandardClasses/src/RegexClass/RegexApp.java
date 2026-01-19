15a25d7545127bbb35706b242ce3e11bc7d8555d
Tonni1973 <tonniramdani@gmail.com>
2026-01-20 05:43:58 +0700

    day 47 java standard classes finish

---
+++ Language/Java/Standard Classes/StandardClasses/src/RegexClass/RegexApp.java
@@ -0,0 +1,18 @@
+package RegexClass;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexApp {
+    public static void main(String[] args) {
+        String name = "Tonani Ramdani Mubaroq Programmer Handal a";
+
+        Pattern pattern = Pattern.compile("[a-zA-Z]*[a][a-zA-Z]*");
+
+        Matcher matcher = pattern.matcher(name);
+
+        while(matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
