a8c312e76025c98f2c03a3686786e038fa70e819
Tonni1973 <tonniramdani@gmail.com>
2026-02-02 16:46:02 +0700

    day 60 java generic

--- Language/Java/Java Generic/JavaGeneric/src/typeerasure/TypeErasureApp.java
+++ Language/Java/Java Generic/JavaGeneric/src/typeerasure/TypeErasureApp.java
@@ -2,7 +2,11 @@ package typeerasure;
 
 public class TypeErasureApp {
     public static void main(String[] args) {
+        // type erasure --> menghilangkan type genericnya
+        Data stringData = new Data<>("Toni");
 
+        Data<Integer> integerData = (Data<Integer>) stringData;
+//        Integer integer = integerData.getData(); // error saat runtime
     }
 
     public static class Data<T> {
