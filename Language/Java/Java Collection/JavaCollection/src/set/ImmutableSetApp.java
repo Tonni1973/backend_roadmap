ff9ab2e0fff96d0b20cb4e593b267e999b0a2de4
Tonni1973 <tonniramdani@gmail.com>
2026-02-12 14:58:12 +0700

    day 70 java collection

---
+++ Language/Java/Java Collection/JavaCollection/src/set/ImmutableSetApp.java
@@ -0,0 +1,12 @@
+package set;
+
+import java.util.Set;
+
+public class ImmutableSetApp {
+    public static void main(String[] args) {
+
+        Set<String> names = Set.of("Budi", "Rahma", "Udin");
+
+//        names.add("Eko");
+    }
+}
