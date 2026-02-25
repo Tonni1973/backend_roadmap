26e1a8ee3f70791e210f521ed7121dfa4080e7ac
Tonni1973 <tonniramdani@gmail.com>
2026-02-25 10:48:01 +0700

    day 81 repeat java OOP

--- Language/Java/OOP/JavaOop/src/toni/RecordClass/RecordApp.java
+++ Language/Java/OOP/JavaOop/src/toni/RecordClass/RecordApp.java
@@ -5,8 +5,13 @@ public class RecordApp {
 
         LoginRequest loginRequest = new LoginRequest("Nuna", "rahasia");
 
+        LoginRequest loginRequestDefault = new LoginRequest("Nuna");
+
         System.out.println(loginRequest.username());
         System.out.println(loginRequest.password());
         System.out.println(loginRequest);
+
+        System.out.println(loginRequestDefault.username());
+        System.out.println(loginRequestDefault);
     }
 }
