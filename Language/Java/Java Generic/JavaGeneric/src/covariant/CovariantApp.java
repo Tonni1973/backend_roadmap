616723b0b1188e706dc6ccf26b0420a3243772da
Tonni1973 <tonniramdani@gmail.com>
2026-03-08 05:49:09 +0700

    day 92 java generic repat

--- Language/Java/Java Generic/JavaGeneric/src/covariant/CovariantApp.java
+++ Language/Java/Java Generic/JavaGeneric/src/covariant/CovariantApp.java
@@ -1,16 +1,24 @@
 package covariant;
 
 import Invariant.MyData;
+import MultipeBoundedTypeParameter.MultipleConstraintApp;
 
 public class CovariantApp {
     public static void main(String[] args) {
-        MyData<String> data = new MyData<>("Tonni");
+        MyData<String> data = new MyData<>("Budi");
         process(data);
+
+        MyData<? extends Object> data1 = new MyData<>("Budi");
+        Object obj = data1.getData();
 
     }
 
     public static void process(MyData<? extends Object> data){
         Object object = data.getData();
         //data.setData("Bob"); // karena bersifat read only
+    }
+
+    public static void init(MyData<? extends Object> data){
+        Object obj = data.getData();
     }
 }
